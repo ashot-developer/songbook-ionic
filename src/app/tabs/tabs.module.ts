605f2f4f3ddbd9326f00a027469817e1b2888d1c
Ashot Markosyan <markosyans.ashot@gmail.com>
2025-04-12 01:06:21 +0400

    added list component

--- src/app/tabs/tabs.module.ts
+++ src/app/tabs/tabs.module.ts
@@ -6,13 +6,15 @@ import { FormsModule } from '@angular/forms';
 import { TabsPageRoutingModule } from './tabs-routing.module';
 
 import { TabsPage } from './tabs.page';
+import { ListComponentModule } from '../list/list.module';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    TabsPageRoutingModule
+    TabsPageRoutingModule,
+    ListComponentModule
   ],
   declarations: [TabsPage]
 })
